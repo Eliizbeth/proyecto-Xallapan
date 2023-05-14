7d4031d09b01d291ea2ff5526f18b5e949a7b29d
Eliizbeth <liizjos.47@gmail.com>
2023-05-13 20:41:36 -0600

    Modificación elimnación de cliente a deshabilitar cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Cliente extends Model
 {
+    use softDeletes;
+
     protected $fillable = [
         'NoCliente',
         'nombre',
